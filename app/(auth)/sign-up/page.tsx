c7d757b962de21704e62762bb2a4b1fb3a45c8d8
Hasham Sarwar <hsarwar4227@gmail.com>
2025-11-16 11:20:07 +0000

    feat: install t-react-hook-form, t-resolvers, t-zod, and t-sonner dependencies

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -10,11 +10,27 @@ import { Label } from '@/components/ui/label'
 import { cn } from '@/lib/utils'
 import { ArrowRight } from 'lucide-react'
 import Link from 'next/link'
+import {useForm} from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import {z} from 'zod'
+
 
 
 
 const Page = () => {
+  const AuthCredientialsValidator = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, {
+      message: 'Password must be at least 8 character longs',
+    })
+  })
 
+  type TAuthCredientialsValidator = z.infer<typeof AuthCredientialsValidator>
+
+  const {register, handleSubmit, formState: { errors }} = useForm({
+    resolver: zodResolver(AuthCredientialsValidator)
+  });
 
   return (
     <>
@@ -38,13 +54,14 @@ const Page = () => {
           </div>
 
           <div className='grid gap-6'>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className='grid gap-2'>
                 <div className='grid gap-1 py-2'>
                   <Label htmlFor='email'>Email</Label>
                   <Input
+                  {...register('email')}
                     className={cn({
-                      'focus-visible:ring-red-500':true,
+                      'focus-visible:ring-red-500':errors.email,
                     })}
                     placeholder='you@example.com'
                   />
@@ -54,9 +71,10 @@ const Page = () => {
                 <div className='grid gap-1 py-2'>
                   <Label htmlFor='password'>Password</Label>
                   <Input
+                  {...register('password')}
                     type='password'
                     className={cn({
-                      'focus-visible:ring-red-500':true
+                      'focus-visible:ring-red-500':errors.password,
                     })}
                     placeholder='Password'
                   />
