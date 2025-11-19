1744b0e2274c668da7b40e31d70a047356e256cf
Hasham Sarwar <hsarwar4227@gmail.com>
2025-11-19 17:01:38 +0000

    feat(server): add middleware to server.ts

--- server.ts
+++ server.ts
@@ -2,10 +2,17 @@ import { getPayloadClient } from "./get-payload";
 
 import express from 'express';
 import { nextApp, nextHandler } from "./next-utils";
+import { router } from "./trpc/trpc";
+import { appRouter } from "./trpc";
+import * as trpcExpress from '@trpc/server/adapters/express';
 
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
+
+const createContext = ({req, res}: trpcExpress.CreateExpressContextOptions) => ({
+    req, res
+})
 
 const start = async() => {
     const payload = await getPayloadClient({
@@ -17,6 +24,10 @@ const start = async() => {
         }
     })
 
+    app.use('/api/trpc', trpcExpress.createExpressMiddleware({
+        router: appRouter,
+        createContext,
+    }))
     app.use((req, res)=>nextHandler(req, res));
     nextApp.prepare().then(()=>{
         // payload.logger.info('Next-js started');
